23709fd4d70448baf8480b0f425c4120b9e1ff00
Ryo Katsuma <katsuma@gmail.com>
2024-06-13 00:23:37 +0900

    Set photos to Hobby

--- src/components/Home/Hobby.tsx
+++ src/components/Home/Hobby.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
+
 export default function Hobby() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32">
+    <section className="w-full py-12 md:py-24 lg:py-24">
       <div className="container mx-auto px-10 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2 my-8">
@@ -9,20 +11,81 @@ export default function Hobby() {
         </div>
         <div className="mx-auto grid items-start gap-8 sm:max-w-4xl sm:grid-cols-2 md:gap-12 lg:max-w-5xl lg:grid-cols-3">
           <div className="grid gap-1">
-            <h3 className="text-lg font-bold">Design Tools</h3>
-            <p className="text-sm text-gray-500 dark:text-gray-400">Figma, Adobe XD, Sketch</p>
+            <Image
+              alt="生ハムとカマンベール"
+              className="object-cover"
+              src="/hobby/dsc02145.jpg"
+              style={{
+                aspectRatio: (2049/1606),
+                objectFit: "cover",
+              }}
+              width={2049}
+              height={1606} />
           </div>
+
           <div className="grid gap-1">
-            <h3 className="text-lg font-bold">Design Principles</h3>
-            <p className="text-sm text-gray-500 dark:text-gray-400">
-              User-centered design, visual hierarchy, typography, color theory
-            </p>
+            <Image
+              alt="キャラメルシュークリーム"
+              className="object-cover"
+              src="/hobby/dsc02004.jpg"
+              style={{
+                aspectRatio: (2049/1606),
+                objectFit: "cover",
+              }}
+              width={2049}
+              height={1606} />
           </div>
+
           <div className="grid gap-1">
-            <h3 className="text-lg font-bold">Soft Skills</h3>
-            <p className="text-sm text-gray-500 dark:text-gray-400">
-              Problem-solving, communication, adaptability, creativity
-            </p>
+            <Image
+              alt="おはぎ"
+              className="object-cover"
+              src="/hobby/dsc01965.jpg"
+              style={{
+                aspectRatio: (2049/1606),
+                objectFit: "cover",
+              }}
+              width={2049}
+              height={1606} />
+          </div>
+
+          <div className="grid gap-1">
+            <Image
+              alt="あんバターサンド"
+              className="object-cover"
+              src="/hobby/dsc01904.jpg"
+              style={{
+                aspectRatio: (2049/1606),
+                objectFit: "cover",
+              }}
+              width={2049}
+              height={1606} />
+          </div>
+
+          <div className="grid gap-1">
+            <Image
+              alt="吉はし深山桜"
+              className="object-cover"
+              src="/hobby/dsc01871.jpg"
+              style={{
+                aspectRatio: (2049/1606),
+                objectFit: "cover",
+              }}
+              width={2049}
+              height={1606} />
+          </div>
+
+          <div className="grid gap-1">
+            <Image
+              alt="大納言"
+              className="object-cover"
+              src="/hobby/dsc01617.jpg"
+              style={{
+                aspectRatio: (2049/1606),
+                objectFit: "cover",
+              }}
+              width={2049}
+              height={1606} />
           </div>
         </div>
       </div>
